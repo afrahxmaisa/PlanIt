1e54630bac19f4d8803044babd3b94f7f46f7145
Hafsa Gaballa <hhg26@drexel.edu>
2023-03-11 15:02:13 -0500

    fixed py codes

--- login.py
+++ login.py
@@ -8,7 +8,7 @@ def login():
 
 
 def signup():
-    email = input("Enter email address: ")
+    stored_email = input("Enter email address: ")
     pwd = input("Enter password: ")
     conf_pwd = input("Confirm password: ")
 
@@ -17,21 +17,21 @@ def signup():
         hash1 = hashlib.md5(enc).hexdigest()
 
         with open("credentials.txt", "w") as f:
-            f.write(email +"\n")
+            f.write({stored_email} +"\n")
             f.write(hash1)
         f.close()
         print("You have registered successfully")
+        return stored_email
     else:
         print("Password does not match")
-
-def login():
+def login(stored_email):
     email = input("Enter email: ")
     pwd = input("Enter password: ")
     
     auth = pwd.encode()
     auth_hash = hashlib.md5(auth).hexdigest()
     with open("credentials.txt", "r") as f:
-        stored_email,stored_pwd== f.read().split("\n")
+        stored_pwd = f.read().split("\n")
     f.close()
 
     if email == stored_email and auth_hash == stored_pwd:
